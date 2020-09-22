a60964fd17528a47e8f7fadef0e389de5789e197
Nguyen Thanh Linh <nguyenthanhlinh29092000@gmail.com>
2020-09-22 17:18:22 +0700

    change code block element

--- src/App.java
+++ src/App.java
@@ -1,19 +1,24 @@
 public class App {
     public static void main(String[] args) throws Exception {
         double value1 = 100.0d;
-        double value2 = 50.0d;
+        double value2 = 0.0d;
         double result = 0.0d;
-        String option = "a";
+        String option = "e";
         if (option == "a")
             result = value1 + value2;
         else if (option == "s")
             result = value1 - value2;
         else if (option == "m")
             result = value1 * value2;
-        else if (option == "d")
-            result = value1 / value2;
-        else
+        else if (option == "d") {
+            if (value2 != 0) {
+                result = value1 / value2;
+            }
+        } else {
+            System.out.println("Invalid option: " + option);
             result = 0.0d;
+
+        }
         System.out.println(result);
     }
 }
